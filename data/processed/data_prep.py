ed9a4d52c3a9918323a82ff9a0f88874148cff08
Axeloums <axelhau@gmail.com>
2024-09-27 15:13:48 +0200

    all data add

---
+++ data/processed/data_prep.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df_main = pd.read_csv(
+    r"C:\Users\Axel\Desktop\Data_Project\OC_Project_9\data\raw\DisponibiliteAlimentaire_2017.csv"
+)
+df_main
--- src/data/data_prep.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]})
-df
